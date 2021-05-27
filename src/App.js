9f99ea8441de7c1698d9af3953ffe7db03929c93
Kevin StOnge <kevinstonge@gmail.com>
2021-05-27 15:58:17 -0400

    requestAnimationFrame implementation working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import Perlin from "./components/Perlin.js";
+import PerlinSI from "./components/PerlinSI.js";
+import PerlinCSS from "./components/PerlinCSS.js";
+import PerlinRAF from "./components/PerlinRAF.js";
+import { useState } from "react";
 import "./App.css";
 function App() {
+  const [implementation, setImplementation] = useState("RAF");
   return (
     <>
-      {/* <Perlin {...{ xDots: 20, yDots: 20, width: 800, height: 800 }}/> */}
+      {/* <Perlin {...{ density: 6, width: 800, height: 800 }}/> */}
       <h1>Perlin Noise Landscape</h1>
-      <Perlin />
+      <div className="button-row">
+        <p>implementation: </p>
+        <button onClick={() => setImplementation("SI")}>
+          setInterval (100ms)
+        </button>
+        <button onClick={() => setImplementation("CSS")}>
+          css transitions (1/sec)
+        </button>
+        <button onClick={() => setImplementation("RAF")}>
+          requestAnimationFrame
+        </button>
+      </div>
+      {implementation === "SI" && <PerlinSI />}
+      {implementation === "CSS" && <PerlinCSS />}
+      {implementation === "RAF" && <PerlinRAF />}
     </>
   );
 }
